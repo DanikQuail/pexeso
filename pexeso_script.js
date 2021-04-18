5de267fb09037cc1dd22e503958094411ad141a7
d1saabl3 <73097704+d1saabl3@users.noreply.github.com>
2021-04-18 19:01:09 +0200

    Hotovo

--- pexeso_script.js
+++ pexeso_script.js
@@ -3,18 +3,18 @@ let pexesoKarty2 = pexesoKarty.length
 
 let nactise = function () {
     pexeso_deska = document.getElementById("boardPexeso")
-    pole_zdvojnasobeni(pexesoKarty);
-    pole_nahodneRozhazeni(pexesoKarty);
+    pole_zdvojnasobeni();
+    pole_nahodneRozhazeni();
     karticky();
 }
 
-let pole_zdvojnasobeni = function (pexesoKarty) {
+let pole_zdvojnasobeni = function () {
     for (let i = 0; i < pexesoKarty2; i++) {
         pexesoKarty.push(pexesoKarty[i])
     }
 }
 
-let pole_nahodneRozhazeni = function (pexesoKarty2) {
+let pole_nahodneRozhazeni = function () {
     for (let i = 0; i < pexesoKarty.length; i++) {
         let nahodnyShuffle = Math.floor(Math.random() * i);
 
